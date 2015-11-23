89dff809b1914a869309aeb29a94cf17f9460984
JorgeCalvo <jorge.calvo@tektonlabs.com>
2015-11-22 20:28:29 -0500

    fix on members of group

--- app/views/admin/tasks/add_member.js
+++ app/views/admin/tasks/add_member.js
@@ -1,3 +1,3 @@
 alert("Task was assigned successfully!");
-$('#members-on-task').html('<%=escape_javascript render partial: "members_on_task", locals: { group: @group, task: @task } %>');
+$('#members-on-task-<%= @task.id %>').html('<%=escape_javascript render partial: "members_on_task", locals: { group: @group, task: @task } %>');
 $('#modal-add-member-task-<%= @task_id %>').modal('hide');
--- app/views/admin/tasks/destroy_assigned_member.js
+++ app/views/admin/tasks/destroy_assigned_member.js
@@ -1,1 +1,1 @@
-$('#members-on-task').html('<%=escape_javascript render partial: "members_on_task", locals: { group: @group, task: @task } %>');
+$('#members-on-task-<%= @task.id %>').html('<%=escape_javascript render partial: "members_on_task", locals: { group: @group, task: @task } %>');
