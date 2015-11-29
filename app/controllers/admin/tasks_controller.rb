db1d4aa93eeec686c632ce301bafd7cdc85fd90a
JorgeCalvo <jorge.calvo@tektonlabs.com>
2015-11-28 20:02:57 -0500

    comments on admin portal

--- app/controllers/admin/tasks_controller.rb
+++ app/controllers/admin/tasks_controller.rb
@@ -48,17 +48,12 @@ module Admin
     def update_status
       @task = Task.find(params[:id])
       @group = @task.work_group
-      p params[:status]
       a = Hash.new
       a["status"] = params[:status].to_i
       @task.update(a)
-
-      p @group
-
       @future_tasks = @group.tasks.future
       @in_progress_tasks = @group.tasks.in_progress
       @done_tasks = @group.tasks.done
-      # @tas.update(status: params[:status])
     end
 
     def destroy_assigned_member
